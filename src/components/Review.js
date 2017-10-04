dccbdbb94958602eec4ef70cb220771fd921a2b4
king0120 <king0120@gmail.com>
2017-10-04 15:58:55 -0400

    Inclass example

---
+++ src/components/Review.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Review extends Component {
+  render(){
+    return (
+      <div>
+        <h1>I hated this movie</h1>
+        <p>By: Jamie</p>
+      </div> 
+    )
+  }
+}
+
+export default Review
